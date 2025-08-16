26493508afbc159f7d252e258923ee765f5ef8ef
jeloualonzo <alonzojelou06@gmail.com>
2025-08-16 19:51:52 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,34 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import mkcert from 'vite-plugin-mkcert';
+import type { UserConfig, Plugin } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
+export default defineConfig(({ mode }): UserConfig => {
+  const plugins = [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    mode === 'development' && componentTagger(),
+    mkcert()
+  ].filter(Boolean) as Plugin[];
+
+  return {
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      https: {},
+      strictPort: true,
+      hmr: {
+        protocol: 'wss',
+        host: 'localhost',
+        port: 5173
+      }
     },
-  },
-}));
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
